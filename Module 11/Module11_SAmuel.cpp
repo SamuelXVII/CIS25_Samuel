f5ce791da654159e145183b28e4e2fbeb2cdaed0
SamuelXVII <sasmamaw41@gmail.com>
2026-05-10 19:32:19 -0700

    Add files via upload

--- Module 11/Module11_SAmuel.cpp
+++ Module 11/Module11_SAmuel.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+struct Patient
+{
+    int id,age;
+    string name;
+};
+
+int main() {
+    Patient patient1;
+    patient1.id = 50;
+    patient1.name = "Thragg";
+    patient1.age = 2000;
+    Patient*p = &patient1;
+
+
+    //Pointer commands
+    cout << "\nID: " << p->id << endl;
+    cout << "Name: "<< p->name <<"\n";
+    cout << "Age: "<< p->age <<"\n\n";
+    return 0;
+}
